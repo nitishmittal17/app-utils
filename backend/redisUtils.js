8ed5579d3d65a58e3d1ddcfe65d3936623e272dd
nitishmittal17 <nitish.mittal.17@gmail.com>
2019-01-25 21:12:07 +0530

    added redis utils

---
+++ backend/redisUtils.js
@@ -0,0 +1,92 @@
+const redis = require('redis');
+
+module.exports = (config, logger) => {
+
+	const logError = (err) => {
+		if (logger && logger.logError) {
+			logger.logError(err);
+		}
+	};
+
+	const log = (type) => {
+		return () => {
+			logError(arguments);
+		}
+    };
+
+	const client = redis.createClient(config.port, config.host, {
+		retry_strategy: function(options) {
+			//Retry interval
+			return config.retryInterval || 300;
+		}}
+	);
+	client.on('reconnecting', log('reconnecting'));
+	client.on('error'       , log('error'));
+	client.on('end'         , log('end'));
+
+    return {
+	    setItem: function(key, value) {
+		    client.set(key, value);
+	    },
+
+	    setItemWithExpiry: function(key, value, expiryTime) {
+		    client.setex(key, expiryTime, value);
+	    },
+
+	    getItem: thunkify(function(key, callback) {
+		    client.get(key, function(err, res) {
+			    if(err) {
+				    logError(err);
+			    }
+
+			    try {
+				    callback(err, JSON.parse(res));
+			    } catch(e) {
+				    callback(err, res);
+			    }
+
+		    });
+	    }),
+
+	    getItemWithCallback: function(key, callback) {
+		    client.get(key, function(err, res) {
+			    if(err) {
+				    logError(err);
+			    }
+			    callback(err, res);
+		    });
+	    },
+
+	    deleteItem: function(key) {
+		    client.del(key, function(err, reply) {
+			    if(err) {
+				    logError(err);
+			    }
+		    });
+	    },
+
+	    deleteItemThunked: thunkify(function(key, callback) {
+		    client.del(key, function(err, res) {
+			    if(err) {
+				    logError(err);
+			    }
+
+			    callback(err, res);
+		    });
+	    }),
+
+	    addToSortedSet: function(setName, score, key) {
+		    client.zadd(setName, score, key);
+	    },
+
+	    getFromSortedSet: thunkify(function(setName, numOfElements, callback) {
+		    client.zrevrange(setName, 0, numOfElements - 1, 'withscores', function(err, res) {
+			    if(err) {
+				    logError(err);
+			    }
+
+			    callback(err, res);
+		    })
+	    })
+    }
+};
